5883a9fe1edf0a2595a9e87731b07bab12fae08a
regisdantas <dantas.regis.m@gmail.com>
2022-08-24 18:16:41 -0300

    Added typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,24 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import routes from './routes'
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use((error: Error, req: Request, resp: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return resp
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message });
+  }
+  console.log(error);
+  return resp.status(500).json({ status: 'error', message: 'Internal error.' });
+});
 
 app.listen(3333, () => {
   console.log('Server started in http://localhost:3333');
