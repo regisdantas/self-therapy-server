dcb653cfa66f8129f57920f23a13ebe7deb75d34
regisdantas <dantas.regis.m@gmail.com>
2022-09-02 13:58:53 -0300

    Adding degug

--- src/modules/users/controllers/SessionsController.ts
+++ src/modules/users/controllers/SessionsController.ts
@@ -4,8 +4,10 @@ import CreateSessionService from '../services/CreateSessionService';
 class SessionsController {
   public async create(req: Request, resp: Response): Promise<Response> {
     const {email, password} = req.body;
+    console.log("Request to create session:", email);
     const CreateSession = new CreateSessionService();
     const result = await CreateSession.execute({email, password});
+    console.log("Request result:", result);
     return resp.json(result);
   }
 }
